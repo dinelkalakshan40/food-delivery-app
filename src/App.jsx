bc75ea8cb5aa6bf1066055c1e45e6c608ce43a11
dinelka <newlakshan3@gmail.com>
2025-05-05 13:30:45 +0530

    add page header

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
 import ExploreFood from "./pages/ExploreFood.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
     <div>
       <Menubar/>
+
       <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/contact' element={<Contact/>}/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import Header from "../components/Header.jsx";
 
 
 const Home=()=>{
     return(
-        <div>Home</div>
+        <main className="container">
+            <Header/>
+        </main>
     )
 }
 export default Home;
