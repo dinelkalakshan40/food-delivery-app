75d47142f7dbac5ea93e3de9a99b044755b8a49f
dinelka <newlakshan3@gmail.com>
2025-05-05 15:41:58 +0530

    create scroll bar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,35 @@
 import logo from './logo-food-removebg-preview.png';
 import cart from './add_4221902.png';
+import biryani from './foodImages/biriyani.jpeg'
+import rolls from './foodImages/rolls.jpeg';
+import pizza from './foodImages/pizza.jpeg'
+import hotdog from './foodImages/hot-dog.jpeg'
+import icecream from './foodImages/ice-creams.jpeg'
+import buger from './foodImages/buger.jpg'
 
 export const assets ={
     logo,
     cart
 }
+export const categories=[
+    {
+        category: 'Biryani',
+        icon: biryani
+    },
+    {
+        category:'Burger',
+        icon:buger
+    },
+    {
+        category:'Rolls',
+        icon:rolls
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    },
+    {
+        category: 'Ice Creams',
+        icon: icecream
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header.jsx";
+import ExploreMenu from "../components/ExploreMenu.jsx";
 
 
 const Home=()=>{
     return(
         <main className="container">
             <Header/>
+            <ExploreMenu/>
         </main>
     )
 }
