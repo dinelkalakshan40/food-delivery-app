1b20ee8781537f59dfad84f97386fcca56b0e9fc
dinelka <newlakshan3@gmail.com>
2025-05-06 23:37:01 +0530

    add searchbar to explore tab

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -2,7 +2,33 @@ import FoodDisplay from "../components/FoodDisplay.jsx";
 
 const ExploreFood=()=>{
     return(
-       <FoodDisplay/>
+
+        <>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <form>
+                            <div className="input-group mb-3 mt-2">
+                                <select className="form-select mt-2" style={{'maxWidth': '150px'}}>
+                                    <option value="Biryani">Biryani</option>
+                                    <option value="Burger">Burger</option>
+                                    <option value="Ice Creams">Ice Creams</option>
+                                    <option value="Pizza">Pizza</option>
+                                    <option value="Rolls">Rolls</option>
+                                </select>
+                                <input type="text" className="form-control mt-2" placeholder="Search a Food..."/>
+                                <button className="btn btn-primary mt-2" type="submit">
+                                    <i className="bi bi-search"></i>
+                                </button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <FoodDisplay/>
+        </>
+
     )
 }
 export default ExploreFood;
